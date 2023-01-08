f78ff7b58e5e9b1bc67070fb873e1410c07c3eab
Ghost_chu <ghostchu@111.com>
2023-01-09 01:10:13 +0800

    comment methods that refer invalid classes

--- src/main/java/com/loohp/interactivechatdiscordsrvaddon/api/InteractiveChatDiscordSrvAddonAPI.java
+++ src/main/java/com/loohp/interactivechatdiscordsrvaddon/api/InteractiveChatDiscordSrvAddonAPI.java
@@ -21,16 +21,7 @@
 package com.loohp.interactivechatdiscordsrvaddon.api;
 
 import com.loohp.interactivechatdiscordsrvaddon.InteractiveChatDiscordSrvAddon;
-import com.loohp.interactivechatdiscordsrvaddon.listeners.InboundToGameEvents;
-import com.loohp.interactivechatdiscordsrvaddon.listeners.InboundToGameEvents.DiscordAttachmentData;
 import com.loohp.interactivechatdiscordsrvaddon.resources.ResourceManager;
-import com.loohp.interactivechatdiscordsrvaddon.wrappers.GraphicsToPacketMapWrapper;
-import org.bukkit.entity.Player;
-
-import java.util.Collections;
-import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
 
 public class InteractiveChatDiscordSrvAddonAPI {
 
@@ -54,38 +45,38 @@ public class InteractiveChatDiscordSrvAddonAPI {
         return InteractiveChatDiscordSrvAddon.plugin.isResourceManagerReady() ? InteractiveChatDiscordSrvAddon.plugin.getResourceManager() : null;
     }
 
-    /**
-     * Get all active discord attachments
-     *
-     * @return A mapping of the assigned UUID to the discord attachments
-     */
-    public static Map<UUID, DiscordAttachmentData> getActiveDiscordAttachments() {
-        return Collections.unmodifiableMap(InboundToGameEvents.DATA);
-    }
-
-    /**
-     * Get all active image preview maps
-     *
-     * @return A mapping of currently viewing players to the image preview maps
-     */
-    public static Map<Player, GraphicsToPacketMapWrapper> getActivePlayerImageMapViews() {
-        return Collections.unmodifiableMap(InboundToGameEvents.MAP_VIEWERS);
-    }
-
-    /**
-     * Get the preview image map by the assigned uuid
-     *
-     * @param uuid the uuid of the image wrapper
-     * @return The image preview map (Could be null)
-     */
-    public static GraphicsToPacketMapWrapper getDiscordImageWrapperByUUID(UUID uuid) {
-        Optional<DiscordAttachmentData> opt = InboundToGameEvents.DATA.values().stream().filter(each -> each.getUniqueId().equals(uuid)).findFirst();
-        DiscordAttachmentData data;
-        if (opt.isPresent() && (data = opt.get()).isImage()) {
-            return data.getImageMap();
-        } else {
-            return null;
-        }
-    }
+//    /**
+//     * Get all active discord attachments
+//     *
+//     * @return A mapping of the assigned UUID to the discord attachments
+//     */
+//    public static Map<UUID, DiscordAttachmentData> getActiveDiscordAttachments() {
+//        return Collections.unmodifiableMap(InboundToGameEvents.DATA);
+//    }
+//
+//    /**
+//     * Get all active image preview maps
+//     *
+//     * @return A mapping of currently viewing players to the image preview maps
+//     */
+//    public static Map<Player, GraphicsToPacketMapWrapper> getActivePlayerImageMapViews() {
+//        return Collections.unmodifiableMap(InboundToGameEvents.MAP_VIEWERS);
+//    }
+//
+//    /**
+//     * Get the preview image map by the assigned uuid
+//     *
+//     * @param uuid the uuid of the image wrapper
+//     * @return The image preview map (Could be null)
+//     */
+//    public static GraphicsToPacketMapWrapper getDiscordImageWrapperByUUID(UUID uuid) {
+//        Optional<DiscordAttachmentData> opt = InboundToGameEvents.DATA.values().stream().filter(each -> each.getUniqueId().equals(uuid)).findFirst();
+//        DiscordAttachmentData data;
+//        if (opt.isPresent() && (data = opt.get()).isImage()) {
+//            return data.getImageMap();
+//        } else {
+//            return null;
+//        }
+//    }
 
 }
